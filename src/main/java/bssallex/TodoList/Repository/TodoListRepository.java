32cd704c815da4d2e7e8b0ee225dc0f8333c1910
Alex Bruno <Bssallex@gmail.com>
2025-05-05 22:27:28 -0300

    feat: Cria uma query personalizada na camada de repository

--- src/main/java/bssallex/TodoList/Repository/TodoListRepository.java
+++ src/main/java/bssallex/TodoList/Repository/TodoListRepository.java
@@ -1,9 +1,14 @@
 package bssallex.TodoList.Repository;
 
+import bssallex.TodoList.Entity.Status;
 import bssallex.TodoList.Entity.TodoListEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TodoListRepository extends JpaRepository<TodoListEntity, Long> {
+
+    List<TodoListEntity> findByStatus(Status status);
 }
