d4040fb6b51f637ebdb4b412694a1b22069726be
Alex Bruno <Bssallex@gmail.com>
2025-05-05 22:25:55 -0300

    feat: Finaliza todos os endpoints da camada de controller e define as rotas da aplicação

--- src/main/java/bssallex/TodoList/Controller/TodoListController.java
+++ src/main/java/bssallex/TodoList/Controller/TodoListController.java
@@ -1,9 +1,15 @@
 package bssallex.TodoList.Controller;
 
+import bssallex.TodoList.Request.ListRequest;
+import bssallex.TodoList.Response.ListResponse;
 import bssallex.TodoList.Service.TodoListService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/list")
@@ -11,4 +17,56 @@ import org.springframework.web.bind.annotation.RestController;
 public class TodoListController {
 
     private final TodoListService service;
+
+    @GetMapping
+    public ResponseEntity<List<ListResponse>> listarTarefas(){
+        return ResponseEntity.ok(service.listar());
+    }
+
+    @GetMapping("/pendentes")
+    public ResponseEntity listarTarefasPendentes(){
+        return ResponseEntity.ok(service.listarPendentes());
+    }
+
+    @GetMapping("/concluidos")
+    public ResponseEntity listarTarefasConcluidas(){
+        return ResponseEntity.ok(service.listarConcluidas());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity listarTarefaPorId(@PathVariable Long id){
+
+        return service.listarId(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @PostMapping
+    public ResponseEntity salvarTarefa(@RequestBody ListRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.salvar(request));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity alterarTarefa(@PathVariable Long id, @RequestBody ListRequest request){
+        return service.listarId(id).map(alterar -> {
+            service.alterar(id, request);
+            return ResponseEntity.ok().build();
+        }).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("Id não encontrado"));
+    }
+
+    @PutMapping("/concluir/{id}")
+    public ResponseEntity tarefaConcluida(@PathVariable Long id){
+        return service.listarId(id).map(concluir -> {
+            service.concluirTarefa(id);
+            return ResponseEntity.ok().build();
+        }).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("Id não encontrado"));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deletarTarefa(@PathVariable Long id){
+        return service.listarId(id).map(deletar ->{
+            service.deletar(id);
+            return ResponseEntity.ok().build();
+        }).orElseGet(() -> ResponseEntity.status(HttpStatus.NO_CONTENT).body("Id não encontrado"));
+    }
 }
